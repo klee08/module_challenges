699f591ffaa98a10e8d7c7e153597521ca1608c9
Ken Lee <kenlee08@gmail.com>
2021-10-03 21:41:02 -0400

    Updated version with save_csv function

--- Module2_Challenge/app.py
+++ Module2_Challenge/app.py
@@ -9,6 +9,7 @@ Example:
 import sys
 import fire
 import questionary
+import pandas as pd
 from pathlib import Path
 
 from qualifier.utils.fileio import load_csv
@@ -26,7 +27,6 @@ from qualifier.filters.loan_to_value import filter_loan_to_value
 
 def load_bank_data():
     """Ask for the file path to the latest banking data and load the CSV file.
-
     Returns:
         The bank data from the data rate sheet CSV file.
     """
@@ -101,16 +101,50 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
 
     return bank_data_filtered
 
-
+def save_csv(qualifying_loans):
+    # prompt - ask csv file name and path to save as: 
+    csvpath = questionary.text("Enter an output file path to save the qualifying data as a file(.csv):").ask()
+    csvpath = Path(csvpath)
+    
+    try:
+        # saving the qualifying data as a csv file.
+        # header same as original data
+    
+    
+        header = ['Lender','MaxLoanAmount','MaxLTV','MaxDTI','MinCreditScore','InterestRate']
+    
+        df_qalifyingloans = pd.DataFrame(qualifying_loans, columns = header)
+    
+        # Save as csv
+        df_qalifyingloans.to_csv(csvpath, index = False)
+            
+    except Exception as e:
+        print("Failed saving the output file. {0}".format(e))
+        #raise exceptions error
+    print(f"Successfully saved the output file at {csvpath}")
+    
 def save_qualifying_loans(qualifying_loans):
     """Saves the qualifying loans to a CSV file.
-
     Args:
         qualifying_loans (list of lists): The qualifying bank loans.
     """
-    # @TODO: Complete the usability dialog for savings the CSV Files.
-    # YOUR CODE HERE!
-
+    
+    # Only save prompt if qualifying loans exist (>0)
+    if len(qualifying_loans) > 0 :
+        answer = questionary.text("Would you like to save your qualifying loans? (Enter yes or no)").ask()
+        answer = answer.strip() #Trim whitespaces
+        answer = answer.lower() #Lower case
+        # if answer is yes, then 'save_csv' function
+        if answer == "yes":
+            save_csv(qualifying_loans)  
+        elif answer == "no":
+            print("No: Skipped saving the output file")
+        else:
+            print("Error: Invalid answer (neither yes nor no) - skipped saving the output file ")
+    else:
+        # if not found, then no need to ask prompt
+        print("No data to save (0 qulifying loans)")
+    
 
 def run():
     """The main function for running the script."""
