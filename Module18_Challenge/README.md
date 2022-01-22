ca1800319b2f96f1ab7cbdfc5bf4960daf80b7bc
Ken Lee <kenlee08@gmail.com>
2022-01-22 16:49:19 -0500

    HW18 submission - final

---
+++ Module18_Challenge/README.md
@@ -0,0 +1,59 @@
+# MODULE 18. PyChain Ledger
+build a blockchain-based ledger system with a user-friendly web interface. 
+This ledger should allow partner banks to conduct financial transactions (i.e.) transfer money between senders and receivers) 
+and to verify the integrity of the data in the ledger.
+* 1. Step 1: # Create a Record Data Class
+* 2. Step 2: # Modify the Existing Block Data Class to Store Record Data
+* 3. Step 3: # Add Relevant User Inputs to the Streamlit Interface
+* 4. Step 4: # Test the PyChain Ledger by Storing Records
+_____________________________________________________
+# README ANSWERS
+1. PyChain - Our Blockchain-based ledger System User-friendly web insterface
+- Streamlit run pychain.py
+
+2. Enter values for the sender, receiver, and amount, and then click the "Add Block" button. 
+e.g. Test Input 
+- Sender: Ken Lee
+- Receiver: Kris Hansen
+- Amount: 15000
+e.g. - http://localhost:8504/#the-pychain-ledger
+- ![RUN_SCREEN](https://github.com/klee08/module_challenges/tree/main/Module18_Challenge/Resources/image1.PNG)
+
+3. Verify the block contents and hashes in the Streamlit drop-down menu.
+Take a screenshot of the Streamlit application page, which should detail a blockchain that consists of multiple blocks. 
+- The PyChain Ledger Record List - Added the above input (new record) to the list successfully
+- [LEDGER_LIST](https://github.com/klee08/module_challenges/tree/main/Module18_Challenge/Resources/image2.PNG)
+
+4. Test the blockchain validation process by using the web interface.
+Take a screenshot of the Streamlit application page, which should indicate the validity of the blockchain
+- Block Inspector - Validated the transaction details thru block inspector
+- ![INSPECTOR](https://github.com/klee08/module_challenges/tree/main/Module18_Challenge/Resources/image3.PNG)
+- ![VALIDATOR](https://github.com/klee08/module_challenges/tree/main/Module18_Challenge/Resources/image4.PNG)
+
+_____________________________________________________
+# Technologies
+* 1. Blockchain is a technology that records and shares data over a network, such as the internet (decentralized)
+-  Shared Accounting with Digital Ledgers: Data class (dataclass) is used to create a block in our blockchain and digital ledger
+-  Improved Data Integrity with Hashing (hashlib)
+* 2. Streamlit application turns Python scripts into shareable web apps - a user-friendly webpage interface for a blockchain.
+  
+# Installation Guide
+For this project modeule: we need the following dependencies:
+  pip install streamlit
+
+# Usage
+To run this project load the jupyter notebook forecasting_net_prophet.ipynb and run.
+Required libaray: 
+# Import required libraries
+import streamlit as st
+from datetime import datetime
+from dataclasses import dataclass
+from typing import Any, List
+import pandas as pd
+import hashlib
+
+# Contributers
+Ken Lee
+Columbia FinTech BootCamp
+# Licence
+Open source project, made for educational purposes only
--- Module18_Challenge/pychain.py
+++ Module18_Challenge/pychain.py
@@ -29,47 +29,29 @@ from typing import Any, List
 import datetime as datetime
 import pandas as pd
 import hashlib
-
+import getpass
 ################################################################################
 # Step 1:
 # Create a Record Data Class
+@dataclass
 
-# Define a new Python data class named `Record`. Give this new class a
-# formalized data structure that consists of the `sender`, `receiver`, and
-# `amount` attributes. To do so, complete the following steps:
-# 1. Define a new class named `Record`.
-# 2. Add the `@dataclass` decorator immediately before the `Record` class
-# definition.
-# 3. Add an attribute named `sender` of type `str`.
-# 4. Add an attribute named `receiver` of type `str`.
-# 5. Add an attribute named `amount` of type `float`.
-# Note that you’ll use this new `Record` class as the data type of your `record` attribute in the next section.
-
-
-# @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
 # YOUR CODE HERE
 
+class Record:
+    sender: str
+    receiver: str
+    amount: float
 
 ################################################################################
 # Step 2:
 # Modify the Existing Block Data Class to Store Record Data
 
-# Rename the `data` attribute in your `Block` class to `record`, and then set
-# it to use an instance of the new `Record` class that you created in the
-# previous section. To do so, complete the following steps:
-# 1. In the `Block` class, rename the `data` attribute to `record`.
-# 2. Set the data type of the `record` attribute to `Record`.
-
-
 @dataclass
 class Block:
 
-    # @TODO
-    # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
-
+    record: Record 
     creator_id: int
     prev_hash: str = "0"
     timestamp: str = datetime.datetime.utcnow().strftime("%H:%M:%S")
@@ -80,7 +62,7 @@ class Block:
 
         record = str(self.record).encode()
         sha.update(record)
-
+        
         creator_id = str(self.creator_id).encode()
         sha.update(creator_id)
 
@@ -135,21 +117,23 @@ class PyChain:
 
 ################################################################################
 # Streamlit Code
-
 # Adds the cache decorator for Streamlit
-
 
 @st.cache(allow_output_mutation=True)
 def setup():
     print("Initializing Chain")
+    # will leave this as is - since we already started the chain
     return PyChain([Block("Genesis", 0)])
-
+    #"but, we can initialize with sample input in format"
+    #genesis_block = Block(
+    #    record=Record(sender=1, receiver=2, amount=100)
+    #return PyChain([genesis_block])
+    #return PyChain([Block("Genesis", 0)])
 
 st.markdown("# PyChain")
 st.markdown("## Store a Transaction Record in the PyChain")
 
 pychain = setup()
-
 ################################################################################
 # Step 3:
 # Add Relevant User Inputs to the Streamlit Interface
@@ -164,34 +148,30 @@ pychain = setup()
 # 4. Add an input area where you can get a value for `amount` from the user.
 # 5. As part of the Add Block button functionality, update `new_block` so that `Block` consists of an attribute named `record`, which is set equal to a `Record` that contains the `sender`, `receiver`, and `amount` values. The updated `Block`should also include the attributes for `creator_id` and `prev_hash`.
 
-# @TODO:
 # Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
+st.markdown("# Our blockchain-based ledger system")
+st.markdown("## Enter Your Transaction Below:")
 
-# @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+sender = st.text_input("Sender")
 
-# @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+receiver = st.text_input("Receiver")
 
-# @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+amount = st.text_input("Amount")
 
+# Set up the web app for deployment (including running the StockChain class)
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
     prev_block_hash = prev_block.hash_block()
-
-    # @TODO
     # Update `new_block` so that `Block` consists of an attribute named `record`
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        data=input_data,
-        creator_id=42,
-        prev_hash=prev_block_hash
+        record = Record(sender, receiver, amount),
+        prev_hash=prev_block_hash,
+        creator_id = (getpass.getuser()) #pass creator_id by os getuser
     )
 
     pychain.add_block(new_block)
@@ -217,6 +197,11 @@ st.sidebar.write(selected_block)
 
 if st.button("Validate Chain"):
     st.write(pychain.is_valid())
+    if pychain.is_valid() == True:
+        st.write("Blockchain is Valid - PASS")
+    else:
+        st.write("Blockchain is Invalid - FAIL")
+    #st.write(pychain.is_valid())
 
 ################################################################################
 # Step 4:
